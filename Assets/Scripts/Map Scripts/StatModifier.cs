f5110b1e688368e52e79cdb3112af0a2ceedcdf0
Shantanu Gupta <shantanugupta2811@gmail.com>
2023-04-08 19:25:07 -0400

    map

---
+++ Assets/Scripts/Map Scripts/StatModifier.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* A stat modifier. */
+
+[System.Serializable]
+public class StatModifier {
+
+	public int modifier;
+
+}
