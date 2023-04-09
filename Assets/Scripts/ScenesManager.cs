3ce1f2f0f119d6b545ede27ddd6fd76fce440db2
Shantanu Gupta <shantanugupta2811@gmail.com>
2023-04-09 19:33:54 -0400

    scenechanger button error

--- Assets/Scripts/ScenesManager.cs
+++ Assets/Scripts/ScenesManager.cs
@@ -1,10 +1,9 @@
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneChanger : MonoBehaviour
+public class ScenesManager : MonoBehaviour
 {
 	public void ChangeScene(string sceneName)
 	{
@@ -15,4 +14,3 @@ public class SceneChanger : MonoBehaviour
 		Application.Quit ();
 	}
 }
-
