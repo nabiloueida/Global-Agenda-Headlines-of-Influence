1dd06337e3265a9b57417029a5f6ccfeb1405bfd
Shantanu Gupta <shantanugupta2811@gmail.com>
2023-04-18 00:24:42 -0400

    background sound and map gradient

---
+++ Assets/Scripts/AudioScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioScript : MonoBehaviour
+{
+    private void Awake()
+    {
+        GameObject[] musicObj = GameObject.FindGameObjectsWithTag("GameMusic");
+        if(musicObj.Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(this.gameObject);
+        }
+        
+    } 
+}
